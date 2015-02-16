3cf4727b3d079f7e9589bcc4495c670d83e39e68
Franco <francoarolfo@hotmail.com>
2015-02-16 18:33:51 -0300

    made app responsive

--- hitchhikeGPS.js
+++ hitchhikeGPS.js
@@ -47,20 +47,37 @@ function initializeMap(hitchs) {
   var mapOptions = {
     zoom: 5,
     center: new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    disableDefaultUI: true,
-    zoomControl: true
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    },
+    disableDefaultUI: true
   };
+
+  var mapStyle = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        { "saturation": -83 },
+        { "lightness": 7 }
+      ]
+    }
+  ];
+
+  var styledMap = new google.maps.StyledMapType(mapStyle,
+    {name: "Styled Map"});
 
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
-  var pointArray = new google.maps.MVCArray(hitchs);
+  map.mapTypes.set('map_style', styledMap);
+  map.setMapTypeId('map_style');
 
+  var pointArray = new google.maps.MVCArray(hitchs);
   heatmap = new google.maps.visualization.HeatmapLayer({
     data: pointArray
   });
 
+  heatmap.set('radius', 25);
   heatmap.setMap(map);
 }
 
