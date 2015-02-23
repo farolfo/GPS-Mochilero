9867d584c7e209db7285ab5e1b96161ae622ca6e
Franco <francoarolfo@hotmail.com>
2015-02-23 01:51:00 -0300

    hidden button

--- app/headerCtrl.js
+++ app/headerCtrl.js
@@ -2,11 +2,13 @@
 
 angular.module('hitchhikeGPS.controllers')
 
-.controller('HeaderCtrl', ['$scope', '$modal', function($scope, $modal) {
-  $scope.openHitchhikeModal = function() {
-    $modal.open({
-      templateUrl: 'addHitchhike/addHitchhike.modal.html',
-      controller: 'AddHitchhikeCtrl'
-    });
-  };
+.controller('HeaderCtrl', ['$scope', '$modal', '$state', function($scope, $modal, $state) {
+    $scope.openHitchhikeModal = function() {
+        $modal.open({
+            templateUrl: 'addHitchhike/addHitchhike.modal.html',
+            controller: 'AddHitchhikeCtrl'
+        });
+    };
+
+    $scope.$state = $state;
 }]);
